a70fa82d1af392b4b4cd25ecb885c5f964167108
diegocamy <diegocamyy@gmail.com>
2020-03-16 22:32:49 -0300

    creada la conexion a la base de datos

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const { Pool } = require('pg');
 require('dotenv').config();
+
+const poolConfig = {
+  user: process.env.DBUSER,
+  host: process.env.DBHOST,
+  database: process.env.DBDATABASE,
+  password: process.env.DBPASSWORD,
+  port: process.env.DBPORT
+};
+
+const pool = new Pool(poolConfig);
 
 const app = express();
 
 app.get('/', (req, res) => {
   return res.send('HOLAMUNDO');
+});
+
+app.get('/query', async (req, res) => {
+  try {
+    const query = await pool.query('SELECT * FROM scraps');
+    const array = await query.rows;
+    return res.json(array);
+  } catch (error) {
+    return res.json({ error: error });
+  }
 });
 
 const port = process.env.PORT || 5000;
