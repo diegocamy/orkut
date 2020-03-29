338e66f466d683e76b37406d5fd1121ad989c674
diegocamy <diegocamyy@gmail.com>
2020-03-28 22:43:14 -0300

    Agregadas las rutas para aceptar y rechazar solicitudes de amistad, y para eliminar un amigo

--- db/amigosQueries.js
+++ db/amigosQueries.js
@@ -6,8 +6,8 @@ const enviarSolicitud = async (req, res) => {
     const solicitud = await (
       await pool.query(
         `SELECT * FROM amistades WHERE 
-      id_usuario1='${req.user.id}' AND id_usuario2='${req.params.id}' 
-      OR id_usuario1='${req.params.id}' AND id_usuario2='${req.user.id}'`
+      id_usuario1='${req.user.id}' AND id_usuario2='${req.params.idUsuario2}' 
+      OR id_usuario1='${req.params.idUsuario2}' AND id_usuario2='${req.user.id}'`
       )
     ).rows[0];
 
@@ -19,7 +19,7 @@ const enviarSolicitud = async (req, res) => {
     const query = {
       text:
         'INSERT INTO amistades (id_usuario1,id_usuario2,status,fecha) VALUES ($1,$2,$3,$4)',
-      values: [req.user.id, req.params.id, 0, new Date().toISOString()]
+      values: [req.user.id, req.params.idUsuario2, 0, new Date().toISOString()]
     };
 
     await pool.query(query);
@@ -35,7 +35,8 @@ const enviarSolicitud = async (req, res) => {
 const verSolicitudesPendientes = async (req, res) => {
   try {
     //obtener todas las solicitudes pendientes para el usuario logeado
-    const query = `SELECT nombre, apellido, foto, id_usuario1 as id_solicitante, status, fecha
+    const query = `SELECT 
+     amistades.id AS id_solicitud, nombre, apellido, foto, id_usuario1 AS id_solicitante, status, fecha
      FROM perfiles 
      JOIN usuarios 
      ON usuarios.id = perfiles.id_usuario 
@@ -53,7 +54,59 @@ const verSolicitudesPendientes = async (req, res) => {
   }
 };
 
+const aceptarSolicitud = async (req, res) => {
+  try {
+    await pool.query(
+      `UPDATE amistades SET status = 1 WHERE id='${req.params.idSolicitud}'`
+    );
+    return res.status(200).send('Solicitud Aceptada!');
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ mensaje: 'Ha ocurrido un error', error: error.message });
+  }
+};
+
+const rechazarSolicitud = async (req, res) => {
+  try {
+    await pool.query(
+      `DELETE FROM amistades WHERE id='${req.params.idSolicitud}'`
+    );
+
+    return res.status(200).send('Solicitud Rechazada!');
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ mensaje: 'Ha ocurrido un error', error: error.message });
+  }
+};
+
+const eliminarAmigo = async (req, res) => {
+  try {
+    const respuesta = await (
+      await pool.query(
+        `DELETE FROM amistades 
+      WHERE id_usuario1='${req.user.id}' AND id_usuario2='${req.params.idAmigo}' AND status = 1
+      OR id_usuario1='${req.params.idAmigo}' AND id_usuario2='${req.user.id}' AND status = 1`
+      )
+    ).rowCount;
+
+    if (respuesta === 0) {
+      return res.status(200).send('No existe esa amistad!');
+    }
+
+    return res.status(200).send('Amigo eliminado!');
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ mensaje: 'Ha ocurrido un error!', error: error.message });
+  }
+};
+
 module.exports = {
   enviarSolicitud,
-  verSolicitudesPendientes
+  verSolicitudesPendientes,
+  aceptarSolicitud,
+  rechazarSolicitud,
+  eliminarAmigo
 };
--- routes/amigos.js
+++ routes/amigos.js
@@ -3,18 +3,54 @@ const route = express.Router();
 const passport = require('passport');
 
 const enviarSolicitud = require('../db/amigosQueries').enviarSolicitud;
+const aceptarSolicitud = require('../db/amigosQueries').aceptarSolicitud;
+const rechazarSolicitud = require('../db/amigosQueries').rechazarSolicitud;
+const eliminarAmigo = require('../db/amigosQueries').eliminarAmigo;
 const verSolicitudesPendientes = require('../db/amigosQueries')
   .verSolicitudesPendientes;
 
 // RUTA PRIVADA
 // metodo: POST
 // descripcion: enviar una solicitud de amistad
-// ruta: /api/amigos/enviarSolicitud
+// ruta: /api/amigos/enviarSolicitud/:idUsuario2
 
 route.post(
-  '/enviarSolicitud/:id',
+  '/enviarSolicitud/:idUsuario2',
   passport.authenticate('jwt', { session: false }),
   enviarSolicitud
+);
+
+// RUTA PRIVADA
+// metodo: POST
+// descripcion: aceptar una solicitud de amistad
+// ruta: /api/amigos/aceptarSolicitud/:id
+
+route.post(
+  '/aceptarSolicitud/:idSolicitud',
+  passport.authenticate('jwt', { session: false }),
+  aceptarSolicitud
+);
+
+// RUTA PRIVADA
+// metodo: POST
+// descripcion: rechazar una solicitud de amistad
+// ruta: /api/amigos/rechazarSolicitud/:id
+
+route.post(
+  '/rechazarSolicitud/:idSolicitud',
+  passport.authenticate('jwt', { session: false }),
+  rechazarSolicitud
+);
+
+// RUTA PRIVADA
+// metodo: POST
+// descripcion: eliminar a un amigo
+// ruta: /api/amigos/eliminarAmigo/:idUsuario2
+
+route.post(
+  '/eliminarAmigo/:idAmigo',
+  passport.authenticate('jwt', { session: false }),
+  eliminarAmigo
 );
 
 // RUTA PRIVADA
